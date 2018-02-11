fb1afed86d40f23221faa9dcf76eab024f891aa5
Hareendra Seneviratne <hareendras@millenniumitesp.com>
2018-02-11 20:24:54 +0530

    section 5 lec 58

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const API_KEY = 'cf89e878ad9300a9c9f9d3f960e9627d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
